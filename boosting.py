db77b31dee2dfca58ae50585d6d579b27695c184
csecen <csecen@verizon.net>
2021-06-06 14:35:07 -0400

    Updating distribution

--- boosting.py
+++ boosting.py
@@ -45,18 +45,20 @@ val_X = val[:,:-1]
 val_Y = val[:,None,-1]
 
 
-print(train.shape)
-print(val.shape)
-print(train_X.shape)
-print(val_X.shape)
-print(train_Y.shape)
-print(val_Y.shape)
+# print(train.shape)
+# print(val.shape)
+# print(train_X.shape)
+# print(val_X.shape)
+# print(train_Y.shape)
+# print(val_Y.shape)
 
 
 indices = np.indices((1, split_size))[1,:,:][0]
 
 dist = np.ones(split_size)
 dist *= 1/split_size
+
+print(f'Dist: {dist}')
 
 
 
@@ -69,19 +71,31 @@ for i in range(10):
     sub_train_Y = sub_train[:,-1]
     sub_train_Y = np.expand_dims(sub_train_Y, axis=1)
     
-    dt = DT(sub_train)
+    dt = DT(sub_train, 1)
 
     train_preds = np.apply_along_axis(dt.predict, axis=1, arr=sub_train_X, tree=dt.tree)
     train_preds = np.expand_dims(train_preds, axis=1)
     misclass = sub_train_Y!=train_preds 
     dt_accuracy = sum(~misclass)/len(train_Y)
-    print(f'Decision Tree accuracy: {dt_accuracy}')
-    
-    dt_preds = np.apply_along_axis(dt.predict, axis=1, arr=val_X, tree=dt.tree)
-    dt_preds = np.expand_dims(dt_preds, axis=1)
-    
-#     misclass = Y!=dt_preds 
-    
-    dt_accuracy = sum(val_Y==dt_preds)/len(val_Y)
-    print(f'Decision Tree accuracy: {dt_accuracy}')
-    print()
+    error = sum(misclass)/len(train_Y)
+    if error < 10/11:
+        print(f'Decision Tree error: {error}')
+        print(f'Decision Tree accuracy: {dt_accuracy}')
+        
+        beta = error/(1-error)
+        print(f'Beta: {beta}')
+        
+        dt_preds = np.apply_along_axis(dt.predict, axis=1, arr=train_X, tree=dt.tree)
+        dt_preds = np.expand_dims(dt_preds, axis=1)
+
+        correct = train_Y==dt_preds 
+        correct = correct.flatten()
+        
+        dist = np.where(correct, dist, dist*beta)
+        Z = sum(dist)
+        print(f'Z: {Z}')
+        dist = dist/Z
+
+        dt_accuracy = sum(correct)/len(train_Y)
+        print(f'Decision Tree accuracy: {dt_accuracy}')
+        print()
